23aa7d78c5d60566d8d549aba8293e34b4766749
goculenkoal <goculenko.al@gmail.com>
2018-05-22 19:12:42 +0300

    1.3. JavDoc. Code convetion.[#4087]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,15 @@
 package ru.job4j.calculate;
+/**
+* Calculate.
+*
+* @author Goculenko Alexander (goculenko.al@gmail.com)
+*/
 
 public class Calculate {
+/**
+* Main.
+* @param args - args.
+*/
 	public static void main (String[] args) {
 		System.out.println("Hello World");
 	}
