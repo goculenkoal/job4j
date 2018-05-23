68222aa3275315ddebe9a50e7311c725fb7ad15b
goculenkoal <goculenko.al@gmail.com>
2018-05-24 00:05:08 +0300

    1.4. JUnit [#4122]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -3,15 +3,26 @@ package ru.job4j.calculate;
 * Calculate.
 *
 * @author Goculenko Alexander (goculenko.al@gmail.com)
+* @version $Id$
+* @since 0.1
 */
 
 public class Calculate {
-/**
-* Main.
-* @param args - args.
-*/
-	public static void main (String[] args) {
-		System.out.println("Hello World");
-	}
-}
+    /**
+     * Main.
+     * @param args - args.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 	
+
+    /**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,7 @@
+/**
+ *
+ * @author Goculenko Alexandr (goculenko.al@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.job4j.calculate;
