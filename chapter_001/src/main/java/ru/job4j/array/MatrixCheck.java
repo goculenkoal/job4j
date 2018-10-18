6efe4fc09cb40c6b376231910cded58390f4b53d
goculenkoal <goculenko.al@gmail.com>
2018-10-18 21:41:45 +0300

    upd

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -10,16 +10,13 @@ public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
         for (int i = 1; i < data.length; i++) {
-            if (data[0][0] != data[i][i] || (data[0][0] != data[data.length - 1 - i][i])) {
-                if ((data[data.length - 1 - i][i] != data[data.length - i][i - 1])) {
-
+            if (data[0][0] != data[i][i] || (data[data.length - 1][0] != data[i][data.length - i - 1])) {
                         result = false;
                         break;
                 }
-
             }
-        }
         return result;
+        }
     }
-}
+
 
