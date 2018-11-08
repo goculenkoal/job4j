a370a61c392e1c8a3a78ff00f1756f8747f2093e
goculenkoal <goculenko.al@gmail.com>
2018-11-08 18:20:47 +0300

    upd

--- chapter_002/src/main/java/ru/job4j/profession/Teacher.java
+++
@@ -1,8 +0,0 @@
-package ru.job4j.profession;
-
-public class Teacher {
-	public Student teach(Student student) {
-		return new Student();
-	}
-}
-	
--- chapter_002/src/main/java/ru/job4j/profession/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
-* Professions.
-*
-* @author Goculenko Alexandr (goculenko.al@gmail.com)
-* @version $Id$
-* @since 0.1
-*/
-package ru.job4j.profession;
