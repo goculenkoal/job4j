2143a105e4f44558324ec783e42d13599c304449
Alex Goculenko <goculenko.al@gmail.com>
2020-01-15 00:21:06 +0300

    Телефонный справочник на базе ArrayList [#41598]

--- chapter_002/src/main/java/ru/job4j/collection/UsageArrayList.java
+++ chapter_002/src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -9,17 +9,17 @@ package ru.job4j.collection;
 import java.util.ArrayList;
 
 public class UsageArrayList {
-	/**
-	 * Класс с реализацией тестового варианта ArrayList и выводом @names чз цикл for each
-	 * 
-	 */
-    public static void main (String[] arg) {
+    /**
+     * Класс с реализацией тестового варианта ArrayList и выводом @names чз цикл for each
+     */
+    public static void main(String[] arg) {
         ArrayList<String> names = new ArrayList<String>();
         names.add("test1");
         names.add("test2");
         names.add("test3");
-        for (String prtnames : names)
+        for (String prtnames : names) {
             System.out.println(prtnames);
 
+        }
     }
 }
