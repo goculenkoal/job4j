83a279da2084cc37b89c0bcb9f0bdee144a171ba
Alex Goculenko <goculenko.al@gmail.com>
2020-01-14 19:06:16 +0300

    0. Коллекции, ArrayList, List, Обобщения [#10094]

---
+++ chapter_002/src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -0,0 +1,25 @@
+package ru.job4j.collection;
+
+/**
+ * @author Goculenko Alexandr (goculenko.al@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+
+import java.util.ArrayList;
+
+public class UsageArrayList {
+	/**
+	 * Класс с реализацией тестового варианта ArrayList и выводом @names чз цикл for each
+	 * 
+	 */
+    public static void main (String[] arg) {
+        ArrayList<String> names = new ArrayList<String>();
+        names.add("test1");
+        names.add("test2");
+        names.add("test3");
+        for (String prtnames : names)
+            System.out.println(prtnames);
+
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/collection/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Use ArrayList collection
+*
+* @author Goculenko Alexandr (goculenko.al@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.collection;
